b828933c77e0e2e90f1e34ad26835bb731a8c0a5
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-11-28 11:37:09 -0500

    have fully created the create function but still getting thrown a 'cannot POST /logs' error

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const reactViews = require('express-react-views');
+const mongoose = require('mongoose');
+const Log = require('./models/logs')
+
+
+// CONNECTION TO DATABASE
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+mongoose.connection.once("open",() => {
+    console.log("connected to mongo")
+  })
+
 
 // VIEW ENGINE
 app.set('view engine', 'jsx');
 app.engine('jsx', reactViews.createEngine());
+
+
+// MIDDLEWARE
+app.use(express.urlencoded({extended: false}))
+
 
 // NEW
 app.get('/', (req, res) => {
@@ -14,13 +33,15 @@ app.get('/', (req, res) => {
 
 // CREATE
 app.post('/', (req, res) => {
-    if (req.body.shipIsBroken === "on"){
-        req.body.shipIsBroken = true;
-    } else {
-        req.body.shipIsBroken = false;
-    }
+    req.body.shipIsBroken = req.body.shipIsBroken === 'on' ? true : false;
 
-    res.send(req.body);
+    Log.create(req.body, (err, createdLog) => {
+        if(!err) {
+            res.status(200).redirect('/');
+        } else {
+            res.status(400).send(err);
+        }
+    });
 })
 
 
--- views/New.jsx
+++ views/New.jsx
@@ -5,8 +5,8 @@ class New extends React.Component {
     render() {
         return (
             <form action="/logs" method="POST">
-                <input type="text" name="title" value="Title of Log" /><br />
-                <input type="textarea" name="entry" value="Log Entry" /><br />
+                <input type="text" name="title" placeholder="Title of Log" /><br />
+                <input type="textarea" name="entry" placeholder="Log Entry" /><br />
                 <input type="checkbox" name="shipIsBroken" /><br />
                 <input type="submit" />
             </form>
